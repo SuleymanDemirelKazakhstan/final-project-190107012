a03556d0b88610a0ab277a6124a58b3d685eda8e
Aruzhan Akkoziyeva <190107012@stu.sdu.edu.kz>
2020-11-02 22:37:43 +0500

    not complete array in .js and pics in categories

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
-const menu_btn = document.querySelector(".burger_menu_icon");
-menu_btn.addEventListener('click', menu_appear);
+
+// document.ready(function(){
+//                  (".cat_card").click(function(){
+//                      (".section").load("list-page.html");
+//                  });
+//             });
+
+
+// document.addEventListener('DOMContentLoaded', function(){
+//   const cards = document.querySelectorAll(".cat_card");
+//   for (card of cards){
+//   	card.addEventListener('click', function() {
+//   		document.querySelector(".section").innerHTML = "list-page.html";
+//   	});
+//   }
+// });
+
+
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// // const router = express.Router();
+
+// app.get('/',function(req,res){
+//   res.sendFile(path.join(__dirname+'/project.html'));
+// });
+
+// app.get('/list-page',function(req,res){
+//   res.sendFile(path.join(__dirname+'/list-page.html'));
+// });
+// // app.use(express.static());
+// app.listen(process.env.port || 3000);
+
+// console.log('Running at Port 3000');
+
+
+
+const books_list = [{name:'The Picture of Dorian Gray', author:'Oscar Wilde', image_url:'images/books/dorian-gray.jpg', price: 12.99},{name:'Hamlet', author:'William Shakespeare', image_url:'images/books/hamlet.jpg', price:9.99}, {name:'Jane Eyre', author:'Charlotte Bronte', image_url:'images/books/jane-eyre.jpg', price:13.00}];
